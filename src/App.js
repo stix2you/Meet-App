8c7d948439b108e7fa3303d1eaa5dcfd4c6b4883
Doug Hill <stix2you@gmail.com>
2024-03-24 17:14:28 -0500

    _test_ folder add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-// test commit
-
-import React from 'react';
-import logo from './meet_app_logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+      </div>
+   );
 }
 
 export default App;
