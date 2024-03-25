c89f41c24cc155c6dc0775c7cb789291532095fd
Doug Hill <stix2you@gmail.com>
2024-03-25 13:48:14 -0500

    problem with citysearch.test.js: TypeError: events.map is not a function Test fail

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,6 +1,7 @@
 // checks whether the CitySearch component contains a textbox or not, and whether it contains a list of suggestions or not.
 
 import { render } from '@testing-library/react';
+import { extractLocations, getEvents } from '../api';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
 
@@ -30,5 +31,28 @@ describe('<CitySearch /> component', () => {
       const suggestionList = CitySearchComponent.queryByRole('list');   // initialized after the click as this is when the list is rendered
       expect(suggestionList).toBeInTheDocument();
       expect(suggestionList).toHaveClass('suggestions');
+   });
+
+   test('updates list of suggestions correctly when user types in city textbox', async () => {
+      const user = userEvent.setup();
+      const allEvents = await getEvents();
+      const allLocations = extractLocations(allEvents);
+      CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+
+      // user types "Berlin" in city textbox
+      const cityTextBox = CitySearchComponent.queryByRole('textbox');
+      await user.type(cityTextBox, "Berlin");
+
+      // filter allLocations to locations matching "Berlin"
+      const suggestions = allLocations ? allLocations.filter((location) => {
+         return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
+      }) : [];
+
+      // get all <li> elements inside the suggestion list
+      const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+      expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+      for (let i = 0; i < suggestions.length; i += 1) {
+         expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
+      }
    });
 });
