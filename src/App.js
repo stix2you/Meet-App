e5f4990db7585a236ade226bf368ac9babaaa8a0
Doug Hill <stix2you@gmail.com>
2024-03-25 09:32:46 -0500

    implemented tests in App.test.js for both event-list and city-search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
 import './App.css';
 
 function App() {
    return (
       <div className="App">
+         <CitySearch />
+         <EventList />
       </div>
    );
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,15 @@
-// src/__tests__/App.test.js
-
 import { render } from '@testing-library/react';
 import App from '../App';
 
-describe('<App /> component', () => {
-
+// Test that the App component renders a list of events
+describe('<App /> component', () => {  // Describe the component we are testing
+   
+   test('renders list of events', () => {    // Test that the component renders a list of events
+      const AppDOM = render(<App />).container.firstChild;    // Render the component and get the DOM element
+      expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();    // Check that the DOM element contains an element with the ID 'event-list'
+   });
+   test('render CitySearch', () => {
+      const AppDOM = render(<App />).container.firstChild;
+      expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+   });
 });
