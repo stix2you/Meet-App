ac3b7cede98f23f4168479bb6035af7e3a960c52
Doug Hill <stix2you@gmail.com>
2024-03-25 09:43:05 -0500

    implemented beforeEach in eventList.test.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,12 +2,16 @@ import { render } from '@testing-library/react';
 import EventList from '../components/EventList';
 
 describe('<EventList /> component', () => {
+   let EventListComponent;
+   beforeEach(() => {
+      EventListComponent = render(<EventList />);
+   });
+
    test('has an element with "list" role', () => {
-      const EventListComponent = render(<EventList />);
       expect(EventListComponent.queryByRole("list")).toBeInTheDocument();   // Check that the DOM element contains an element with the ROLE 'list'
    });
    test('renders correct number of events', () => {
-      const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
+      EventListComponent.rerender(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
       expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
    });
 });
