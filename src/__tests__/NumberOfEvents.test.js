628b39b40a3ee036bfbd9ecdc55e1b5c83fe3bc5
stix2you <stix2you@gmail.com>
2024-03-26 17:58:06 -0500

    rewrite textbox changes when user types in it test, in numberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,25 +1,31 @@
-// src/__tests__/NumberOfEvents.test.js
-
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import NumberOfEvents from '../components/NumberOfEvents';
+import userEvent from '@testing-library/user-event';
 
-test('component contains an element with the role of textbox', () => {
-  render(<NumberOfEvents />);
-  const textboxElement = screen.getByRole('textbox');
-  expect(textboxElement).toBeInTheDocument();
-});
+describe('<NumberOfEvents />', () => {
 
-test('default value of the input field is 32', () => {
-  render(<NumberOfEvents />);
-  const inputElement = screen.getByRole('textbox');
-  expect(inputElement.value).toBe('32');
-});
+   beforeEach(() => {
+      render(<NumberOfEvents />);
+   });
 
-test('value of the textbox changes when a user types in it', async () => {
-  render(<NumberOfEvents />);
-  const inputElement = screen.getByRole('textbox');
-  fireEvent.change(inputElement, { target: { value: '10' } });
-  expect(inputElement.value).toBe('10');
+   // test that the component renders an input element of a textbox
+   test('component contains an element with the role of textbox', () => {
+      const inputElement = screen.getByRole('textbox');
+      expect(inputElement).toBeInTheDocument();
+   });
+
+   // test that the default value of the input field is 32
+   test('default value of the input field is 32', () => {
+      const inputElement = screen.getByRole('textbox');
+      expect(inputElement.value).toBe('32');
+   });
+
+   // test that the value of the textbox changes when a user types in it
+   test('value of the textbox changes when a user types in it', async () => {
+      const inputElement = screen.getByRole('textbox');   // screen is a global object provided by the testing library
+      await userEvent.type(inputElement, '{backspace}{backspace}10');  // simulate clear the input field and type '10'
+      expect(inputElement.value).toBe('10');
+    });
 });
