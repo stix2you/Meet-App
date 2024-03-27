257607b5312fb789f5b128397222c8fffc1949bc
stix2you <stix2you@gmail.com>
2024-03-27 13:57:59 -0500

    implement first integration test in EventList.test.js, passes successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useState, useEffect } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents} from './api';
+import './App.css';
 
 function App() {
+   const [events, setEvents] = useState([]);
+   const [currentNOE, setCurrentNOE] = useState(32);
+
+   // populate the events state with the events list we fetch
+   const fetchData = async () => {
+      const allEvents = await getEvents();    // Fetch the list of events 
+      setEvents(allEvents.slice(0, currentNOE));   // Set the events state to the first 32 events (or currentNOE)
+   }
+
+   // Fetch the list of events when the component mounts
+   useEffect(() => { 
+      fetchData();
+   }, []);
+
+   // return the App component with the CitySearch, NumberOfEvents, and EventList components
+   // Pass the events state to the EventList component
    return (
       <div className="App">
          <CitySearch />
-         <EventList />
          <NumberOfEvents />
+         <EventList events={events}/>
       </div>
    );
 }
