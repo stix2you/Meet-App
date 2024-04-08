065cfcf2422e050b734aadc013772cc89cc7e6a4
Doug Hill <stix2you@gmail.com>
2024-04-08 14:20:33 -0500

    remove atatus import

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-import * as atatus from 'atatus-spa';
-atatus.config('a26daf601d514f35988a772a5b454779').install();
+// import * as atatus from 'atatus-spa';
+// atatus.config('a26daf601d514f35988a772a5b454779').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
